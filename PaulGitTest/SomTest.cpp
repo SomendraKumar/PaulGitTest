699192ffd75c6317847e1ddbd4031074ff8f67de
somendrakumar@iscaglobal.org <somendrakumar@iscaglobal.org>
2022-05-09 12:50:42 +0530

    Make changes by me

---
+++ PaulGitTest/SomTest.cpp
@@ -0,0 +1,7 @@
+#include "SomTest.h"
+#include<iostream>
+using namespace std;
+void SomTest::play()
+{
+	cout << "hey i m updated\n";
+}
--- PaulGitTest/Source.cpp
+++ PaulGitTest/Source.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
+#include"SomTest.h"
 using namespace std;
 void main()
 {
+	SomTest s;
+	s.play();
 	cout << "hello game git hub\n";
 }
